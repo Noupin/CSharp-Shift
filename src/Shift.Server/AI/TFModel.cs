1a3a360fabb605c805953bd015ea23bc5704cb4a
Noupin <naperkins7@gmail.com>
2022-01-20 10:24:57 -0500

    updated dependencies

--- src/Shift.Server/AI/TFModel.cs
+++ src/Shift.Server/AI/TFModel.cs
@@ -1,7 +1,22 @@
-﻿namespace Shift.Server.AI
+﻿using static Tensorflow.Binding;
+using static Tensorflow.KerasApi;
+using Tensorflow;
+using Tensorflow.NumPy;
+
+
+
+namespace Shift.Server.AI
 {
-    public class TFModel : Keras.Models.Model
+    /// <summary>
+	/// Base TensorFlow Model for TFModel
+	/// </summary>
+    public class TFModel
     {
+        private IEnumerable<Tensorflow.Keras.ILayer> ModelLayers;
+
+        public TFModel()
+        {
+        }
 
     }
 }
